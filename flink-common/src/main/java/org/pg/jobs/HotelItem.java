2ab09d4cc965d5c9bae8a52ce0eab33b8b638fa7
doubleStarStar <starstar_sun@163.com>
2019-07-15 00:11:25 +0800

    topN find

---
+++ flink-common/src/main/java/org/pg/jobs/HotelItem.java
@@ -0,0 +1,9 @@
+package org.pg.jobs;
+
+/**
+ * @author Starstar Sun
+ * @date 2019/7/14
+ * @Description:
+ **/
+public class Hoteltem {
+}
---
+++ flink-common/src/main/java/org/pg/model/UserBehavior.java
@@ -0,0 +1,9 @@
+package org.pg.model;
+
+/**
+ * @author Starstar Sun
+ * @date 2019/7/14
+ * @Description:
+ **/
+public class UserBehavior {
+}
